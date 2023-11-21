25d096555b2fce0e0eae4d9ed31a423049f3e9da
Lars <36763@ma-web.nl>
2023-11-21 15:02:46 +0100

    01A

--- 01A/app.js
+++ 01A/app.js
@@ -4,7 +4,20 @@ class App
      runApplication()
      {
         console.log("hello world!");
+        let eenVariableNaam = "AHHHHHHHHHH"
+        let versiedatum = "3.2"
+        let autheur = "Lars"
+        let copyright = "mypants"
+        let distributeur = "spongebob"
+        let darkmode = "ok"
         //code gaat hier!
+
+        console.log(eenVariableNaam)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
      }
 }
 

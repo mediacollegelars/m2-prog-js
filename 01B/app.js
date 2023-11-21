3f30411cabbc2b73f708c5cccafa5b9956af9afd
Lars <36763@ma-web.nl>
2023-11-21 15:30:22 +0100

    01B

---
+++ 01B/app.js
@@ -0,0 +1,25 @@
+class App
+{
+
+     runApplication()
+     { 
+      this.greeting = "starting up";
+        //code gaat hier
+       this.eenVariableNaam = "AHHHHHHHHHH"
+         this.versiedatum = "3.2"
+         this.autheur = "Lars"
+         this.copyright = "mypants"
+         this.distributeur = "spongebob"
+         this.darkmode = "ok"
+     }
+}
+
+let app = new App();
+app.runApplication();
+
+console.log(App.greeting)
+console.log("appNaam  " + app.appNaam)
+console.log("autheur  " + app.autheur)
+console.log("copyright  " + app.copyright)
+console.log("distributeur  " + app.distributeur)
+console.log("darkmode  " + app.versienummer)

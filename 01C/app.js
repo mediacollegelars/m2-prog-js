18e35aca79f459ef344a88078ec63c277902db25
Lars <36763@ma-web.nl>
2023-11-23 14:18:38 +0100

    o1c

--- 01C/app.js
+++ 01C/app.js
@@ -3,14 +3,22 @@ class App
 
      runApplication()
      { 
+         //code gaat hier
       this.greeting = "starting up";
-        //code gaat hier
        this.eenVariableNaam = "AHHHHHHHHHH"
          this.versiedatum = "3.2"
          this.autheur = "Lars"
          this.copyright = "mypants"
          this.distributeur = "spongebob"
          this.darkmode = "ok"
+
+         let watermeloen = "ikhebzininwatermeloen"
+         let computer = "cool computer"
+         let trui = "grijs"
+        
+         this. stoel = "wielen"
+         this. wessel = "ja"
+         this. robin = "broodjekaas"
      }
 }
 
@@ -23,3 +31,9 @@ console.log("autheur  " + app.autheur)
 console.log("copyright  " + app.copyright)
 console.log("distributeur  " + app.distributeur)
 console.log("darkmode  " + app.versienummer)
+console.log("watermeloen " + app.watermeloen)
+console.log("computer " + app.computer)
+console.log("trui " + app.trui)
+console.log("stoel " + app.stoel)
+console.log("wessel " + app.wessel)
+console.log("robin " + app.robin)

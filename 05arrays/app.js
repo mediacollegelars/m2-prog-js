c5438640562adda6acd97596736b7c68eec89dfc
Lars <36763@ma-web.nl>
2024-02-01 11:50:40 +0100

    1

--- 05arrays/app.js
+++ 05arrays/app.js
@@ -1,7 +1,7 @@
 class App {
   runApplication() {
       console.log("hello world");
-      const list = ["artist1", "artist2", "artist3"];
+      const list = ["Jay", "Lars", "Wessel"];
       console.log(list);
       for (let i = 0; i < list.length; i++) {
           const element = i +": " + list[i];
@@ -13,18 +13,18 @@ class App {
           const element = nummers[i] + 1;
           console.log(element);
       }
-      list.push("daniel1")
-      list.push("daniel2")
+      list.push("lars1")
+      list.push("lars2")
       for (let i = 0; i < list.length; i++) {
           const element = i +": " + list[i];
           console.log(element);
       }
-      let indexToRemove = list.indexOf("daniel1")
+      let indexToRemove = list.indexOf("Lars")
       list.splice(indexToRemove,1)
-      indexToRemove = list.indexOf("daniel2")
+      indexToRemove = list.indexOf("lars2")
       list.splice(indexToRemove,1)
-      list.push("daniel3")
-      list.push("daniel4")
+      list.push("lars3")
+      list.push("lars4")
       for (let i = 0; i < list.length; i++) {
           const element = i +": " + list[i];
           console.log(element);
